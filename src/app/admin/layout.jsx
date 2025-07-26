42583125275f5b8996a9bef9f04f0533bc52ef15
Rara <deverinth88@gmail.com>
2025-07-27 07:51:53 +0800

    make the adminheader a server c

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -3,6 +3,9 @@ import { AppSidebar } from "@/components/app-sidebar";
 import RightPanel from "./right-panel";
 import AdminLayoutHeader from "@/components/AdminLayoutHeader";
 import { createClient } from "@/utils/supabase/server";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default async function Layout({ children }) {
     const db = await createClient();
@@ -13,16 +16,28 @@ export default async function Layout({ children }) {
     } = await db.auth.getUser();
 
     if (error) {
-        throw new Error(
-            "Please make sure that you have a stable internet connection."
-        );
+        notFound();
     }
+
+    const fallbackUi = (
+        <div className="flex items-center justify-between w-full ms-2">
+            <Skeleton className="h-5 rounded w-32" />
+            <div className="ms-auto flex gap-2">
+                <Skeleton className="h-5 rounded w-5" />
+                <Skeleton className="h-5 rounded w-5" />
+                <Skeleton className="h-5 rounded w-5" />
+                <Skeleton className="h-5 rounded w-16" />
+            </div>
+        </div>
+    );
     return (
         <SidebarProvider>
             <AppSidebar />
             <div className="w-full ">
                 <main className="min-h-screen relative">
-                    <AdminLayoutHeader currentUserId={user.id} />
+                    <Suspense fallback={fallbackUi}>
+                        <AdminLayoutHeader currentUserId={user.id} />
+                    </Suspense>
 
                     {/* layout body  */}
                     <div className="py-3 flex">
--- src/components/AdminLayoutHeader.jsx
+++ src/components/AdminLayoutHeader.jsx
@@ -1,96 +1,90 @@
-"use client";
-
 import { SidebarTrigger } from "./ui/sidebar";
 import Image from "next/image";
 import { ToggleThemeButton } from "./ui/ToggleThemeButton";
-import { createClient } from "@/utils/supabase/client";
 import { Bell } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { Skeleton } from "./ui/skeleton";
-import { toast } from "sonner";
+import { createClient } from "@/utils/supabase/server";
+import { notFound } from "next/navigation";
 
-export default function AdminLayoutHeader({ currentUserId }) {
-    const [userData, setUserData] = useState({});
-    const [loading, setLoading] = useState(true);
+export default async function AdminLayoutHeader({ currentUserId }) {
+    const supabase = await createClient();
 
-    useEffect(() => {
-        const fetchUserData = async () => {
-            const supabase = createClient();
+    const { data, error } = await supabase
+        .from("users")
+        .select("id, firstname, lastname")
+        .single()
+        .eq("id", currentUserId);
 
-            try {
-                const { data, error } = await supabase
-                    .from("users")
-                    .select("id, firstname, lastname")
-                    .single()
-                    .eq("id", currentUserId);
+    if (data.firstname === null) {
+        notFound();
+    }
+    // const [userData, setUserData] = useState({});
+    // const [loading, setLoading] = useState(true);
 
-                if (error) {
-                    toast.error(
-                        "Please make sure that you have a stable internet connection."
-                    );
-                }
-                console.log(data);
-                setUserData(data);
-            } catch (err) {
-                console.error("Admin layout error : ", err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchUserData = async () => {
+    //         const supabase = createClient();
 
-        fetchUserData();
-    }, []);
+    //         try {
+    //             const { data, error } = await supabase
+    //                 .from("users")
+    //                 .select("id, firstname, lastname")
+    //                 .single()
+    //                 .eq("id", currentUserId);
+
+    //             if (error) {
+    //                 toast.error(
+    //                     "Please make sure that you have a stable internet connection."
+    //                 );
+    //             }
+    //             console.log(data);
+    //             setUserData(data);
+    //         } catch (err) {
+    //             console.error("Admin layout error : ", err.message);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+
+    //     fetchUserData();
+    // }, []);
 
     return (
         <div className="flex items-center px-3 py-[15.3px] border-b dark:border-neutral-900 sticky top-0 left-0 bg-background/50 backdrop-blur-2xl w-full md:pe-5 z-50">
             <SidebarTrigger />
-
-            {loading ? (
-                <div className="flex items-center justify-between w-full ms-2">
-                    <Skeleton className="h-5 rounded w-32" />
-                    <div className="ms-auto flex gap-2">
-                        <Skeleton className="h-5 rounded w-5" />
-                        <Skeleton className="h-5 rounded w-5" />
-                        <Skeleton className="h-5 rounded w-5" />
-                        <Skeleton className="h-5 rounded w-16" />
-                    </div>
+            <>
+                <div className="md:flex items-center  gap-2 border-s ps-3 ms-2 hidden">
+                    <p className="font-medium">
+                        Welcome back, {data?.firstname} 👋
+                    </p>
                 </div>
-            ) : (
-                <>
-                    <div className="md:flex items-center  gap-2 border-s ps-3 ms-2 hidden">
-                        <p className="font-medium">
-                            Welcome back, {userData?.firstname} 👋
+                <div className="ms-auto flex items-center gap-3 md:pe-2">
+                    <div className="text-neutral-700 dark:text-neutral-400 flex items-center justify-center">
+                        <Bell
+                            size={20}
+                            className="hover:text-accent-foreground"
+                        />
+                    </div>
+                    <div className="text-neutral-700 dark:text-neutral-400 flex items-center justify-center">
+                        <ToggleThemeButton className="hover:text-accent-foreground" />
+                    </div>
+                    <Link
+                        href={`/user/${data?.id}`}
+                        className="flex items-center justify-center"
+                    >
+                        <Image
+                            src="/default-pic.jpg"
+                            width={25}
+                            height={25}
+                            alt="avatar"
+                            className="rounded-full"
+                        />
+                        <p className="text-xs hidden md:flex px-2">
+                            {data?.firstname} {data?.lastname}
                         </p>
-                    </div>
-                    <div className="ms-auto flex items-center gap-3 md:pe-2">
-                        <div className="text-neutral-700 dark:text-neutral-400 flex items-center justify-center">
-                            <Bell
-                                size={20}
-                                className="hover:text-accent-foreground"
-                            />
-                        </div>
-                        <div className="text-neutral-700 dark:text-neutral-400 flex items-center justify-center">
-                            <ToggleThemeButton className="hover:text-accent-foreground" />
-                        </div>
-                        <Link
-                            href={`/user/${userData?.id}`}
-                            className="flex items-center justify-center"
-                        >
-                            <Image
-                                src="/default-pic.jpg"
-                                width={25}
-                                height={25}
-                                alt="avatar"
-                                className="rounded-full"
-                            />
-                            <p className="text-xs hidden md:flex px-2">
-                                {userData?.firstname} {userData?.lastname}
-                            </p>
-                        </Link>
-                    </div>
-                </>
-            )}
+                    </Link>
+                </div>
+            </>
         </div>
     );
 }
