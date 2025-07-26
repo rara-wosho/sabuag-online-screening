9b54df2bb296f4fb34e47f42f10d5cef6c47269d
Rara <deverinth88@gmail.com>
2025-07-26 11:18:54 +0800

    added suspense wrapper to searchform which uses useSearchParams

--- src/app/admin/applications/layout.jsx
+++ src/app/admin/applications/layout.jsx
@@ -1,11 +1,21 @@
 import SearchForm from "@/components/SearchForm";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
 export default function Layout({ children }) {
+    const fallbackUi = (
+        <div className="flex items-center gap-2">
+            <Skeleton className="w-full h-8" />
+            <Skeleton className="w-12 h-8" />
+        </div>
+    );
     return (
         <div>
             {/* applications search bar  */}
             <div className="flex items-center mb-4 gap-2">
-                <SearchForm />
+                <Suspense fallback={fallbackUi}>
+                    <SearchForm />
+                </Suspense>
             </div>
             {children}
         </div>
