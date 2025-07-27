e167d9ab4e0dd06d303255b4b7cfb75613cb1652
Rara <deverinth88@gmail.com>
2025-07-27 11:54:27 +0800

    added loading.jsx for applications table

--- src/app/admin/applications/loading.jsx
+++ src/app/admin/applications/loading.jsx
@@ -1,7 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
     return (
-        <div className="flex flex-col justify-center items-center min-h-[70vh]">
-            <div className="size-7 border-y-2 rounded-full animate-spin border-white"></div>
+        <div className="flex flex-col min-h-[70vh]">
+            {/* <div className="size-7 border-y-2 rounded-full animate-spin border-white"></div> */}
+            <div className="flex flex-col gap-y-2">
+                <Skeleton className="h-6 w-32 mb-2 rounded" />
+                <Skeleton className="h-8 w-full rounded" />
+                <Skeleton className="h-8 w-full rounded" />
+            </div>
         </div>
     );
 }
